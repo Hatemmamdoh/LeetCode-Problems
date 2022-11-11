7cbc156c769ee7debd9c4beb3e42b9bcd06b3919
Hatem Mamdoh <hm.hatemmamdoh@gmail.com>
2022-11-11 13:36:09 +0200

    Time: 12 ms (8.98%), Space: 49.5 MB (9.68%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -0,0 +1,89 @@
+class Solution {
+     int[] root;
+     char[] charRoot;
+     int[] rank;
+     HashMap<Integer,Integer> unique = new HashMap<>() ;
+     int count = 0 ;
+     HashMap<Integer,List<Integer>> components = new HashMap<>() ;
+     HashMap<Integer,Integer> stop = new HashMap<>() ;
+     int max = Integer.MIN_VALUE;
+    public int maxAreaOfIsland(int[][] grid) {
+        
+        int [] lrud = {-1,1,-1,1} ;
+
+        root = new int [grid.length*grid[0].length] ;
+        rank = new int [grid.length*grid[0].length] ;
+        for (int i = 0 ; i < grid.length ; i++){
+            for (int j = 0 ; j < grid[0].length;j++){
+                if (grid[i][j] == 1){
+                    root[i*grid[0].length+j] = i*grid[0].length+j ;
+                    rank[i*grid[0].length+j] = 1 ;
+                }
+                else {
+                    root[i*grid[0].length+j] = -1 ;
+                }
+            }
+        }
+        for (int i = 0 ; i < grid.length ; i++){
+            for (int j = 0 ; j < grid[0].length;j++){
+                if (grid[i][j] == 1){
+                    if (j+lrud[0] >= 0 && grid[i][j-1] == 1){
+                        union(i*grid[0].length+j,i*grid[0].length+(j-1));
+                    }
+                    if (j+lrud[1] <= grid[0].length-1 && grid[i][j+1] == 1){
+                        union(i*grid[0].length+j,i*grid[0].length+(j+1));
+                    }
+                    if (i+lrud[2] >= 0 && grid[i-1][j] == 1){
+                        union(i*grid[0].length+j,(i-1)*grid[0].length+(j));
+                    }
+                    if (i+lrud[3] <= grid.length-1 && grid[i+1][j] == 1){
+                        union(i*grid[0].length+j,(i+1)*grid[0].length+(j));
+                    }
+                }
+            }
+        }
+        for (int i = 0 ; i < root.length ; i++ ){
+            if (root[i] != -1)
+                root[i] = find(root[i]) ;
+        }
+        for (int i = 0 ; i < root.length ; i++){
+            if (root[i] != -1 ){
+                if (!unique.containsKey(root[i])){
+                    unique.put(root[i],1) ;
+                }
+                else {
+                    unique.put(root[i],unique.get(root[i])+1) ;
+                }
+                max = Integer.max(max,unique.get(root[i])) ;
+
+            }
+        }
+        if (max == Integer.MIN_VALUE) 
+            return 0;
+        else 
+            return max;
+    }
+    public  int find(int z) {
+        while (z != root[z]) {
+            z = root[z];
+        }
+        return z;
+    }
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+                rank[rootX] += rank[rootY];
+            } else if (rank[rootY] > rank[rootX]) {
+                root[rootX] = rootY;
+                rank[rootY] += rank[rootX];
+            } else {
+                root[rootY] = rootX;
+                rank[rootX] += 1;
+            }
+        }
+
+    }
+}
