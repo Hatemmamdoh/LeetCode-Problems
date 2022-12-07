d7ab87fd83f85dd1f045fab7dffac8ec72ef76c5
Hatem Mamdoh <hm.hatemmamdoh@gmail.com>
2022-12-07 11:30:52 +0200

    Time: 2 ms, Memory: 55.6 MB - LeetHub

---
+++ reverse-string/reverse-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s){
+
+        helper(s,0,s.length-1);
+    }
+    public void helper (char[] s , int i , int j){
+        if (i >= j){
+            return  ;
+        }
+        char t = s[i] ;
+        s[i] = s[j] ;
+        s[j] = t ;
+        helper(s,i+1,j-1);
+    }
+}
