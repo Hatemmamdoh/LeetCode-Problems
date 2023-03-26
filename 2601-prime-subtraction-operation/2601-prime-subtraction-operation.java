faa7b01413e048fdae5775d7027ba19b677a6103
Hatem Mamdoh <hm.hatemmamdoh@gmail.com>
2023-03-26 13:40:39 +0200

    Time: 68 ms (45.45%), Space: 42.5 MB (63.64%) - LeetHub

--- 2601-prime-subtraction-operation/2601-prime-subtraction-operation.java
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.java
@@ -1,70 +1,71 @@
 class Solution {
-    
-    ArrayList<Integer> primes = new ArrayList<Integer>();
-    
-    public void getPrimes(){
-        
-        boolean[] visited = new boolean[10001];
-        
-        for(int i = 2; i <= 10000; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                primes.add(i);
-                
-                int j = i;
-                while(j <= 10000){
-                    visited[j] = true;
-                    j += i;
-                }
-            }
-        }
-        
-    }
-    
     public boolean primeSubOperation(int[] nums) {
-        getPrimes();
-        
-        for(int i = 0; i < nums.length; i++){
-            int prime = 0;
-            if(i == 0){
-                for(int j : primes){
-                    if(j >= nums[i]){
-                        break;
-                    }
-
-                    prime = j;
-                }
-
-                nums[i] -= prime;
-
-                // if(prime != 0)
-                //     primes.remove((Integer)prime);
-            }else{
-                for(int j : primes){
-                    if(j >= nums[i]){
-                        break;
-                    }
-
-                    if(nums[i-1] < (nums[i] - j)){
-                        prime = j;
-                    }
-                }
-
-                nums[i] -= prime;
-
-                // if(prime != 0)
-                //     primes.remove((Integer)prime);
+        int max = Integer.MIN_VALUE ;
+        for (int i = 0 ; i < nums.length ; i++){
+            if (nums[i] > max){
+                max = nums[i] ;
+            }
+        }
+        if (sorted(nums,0)){
+            return true ;
+        }
+        ArrayList<Integer> primes = new ArrayList<>() ;
+        for (int i = 2 ; i < max ; i++){
+            if(isPrime(i)){
+                primes.add(i) ;
             }
         }
 
-        System.out.println(Arrays.toString(nums));
+        for (int i = 0 ; i < nums.length;i++){
+            int prime= 0;
 
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i-1] >= nums[i]){
-                return false;
+            if (i == 0 ){
+                for (int j : primes ){
+                    if (j>=nums[i]){
+                        break;
+                    }
+                    prime = j ;
+                }
+                nums[i] = nums[i] - prime ;
             }
-        }
+            else {
+                for (int j : primes ){
+                    if (j>=nums[i]){
+                        break;
+                    }
+                    if (nums[i-1] < nums[i]-j){
+                        prime = j ;
+                    }
+                }
+                nums[i] = nums[i] - prime ;
+            }
 
+            if (sorted(nums,0)){
+                return true ;
+            }
+
+        }
+        return false ;
+    }
+    public  boolean isPrime(int num)
+    {
+        if(num<=1)
+        {
+            return false;
+        }
+        for(int i=2;i<=num/2;i++)
+        {
+            if((num%i)==0)
+                return  false;
+        }
         return true;
+    }
+    public static boolean sorted (int [] nums,int start){
+        for (int i = start ; i < nums.length-1 ; i++){
+            if (nums[i] >= nums[i+1]){
+                return false ;
+            }
+        }
+        return true ;
     }
 }
